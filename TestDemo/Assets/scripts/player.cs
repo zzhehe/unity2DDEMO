d2f96f46df13a64e8634da067da87bdbf0c52cd5
zzhehe <460475523@qq.com>
2018-03-08 00:24:11 +0800

    三连击
    三连击

--- TestDemo/Assets/scripts/player.cs
+++ TestDemo/Assets/scripts/player.cs
@@ -1,36 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
 
     private Animator animator;
+    private AnimatorStateInfo stateInfo;
+
+    private const string IdleState = "Idle";
+    private const string Attack1State = "Attack1";
+    private const string Attack2State = "Attack2";
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         animator = GetComponent<Animator>();
-	}
+        animator.SetBool("Run", false);
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        MovePlayer();
 
+        stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (!stateInfo.IsName(IdleState) && stateInfo.normalizedTime > 0.8f)
+        {
+            animator.SetInteger("Attack", 0);
+        }
         if (Input.GetKey(KeyCode.A))
         {
-            animator.SetBool("Run",true);
+            Attack();
         }
-        else
-        {
-            animator.SetBool("Run", false);
-        }
+    }
 
-        if (Input.GetKeyDown(KeyCode.S))
+    private void MovePlayer()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Attack()
+    {
+        if (stateInfo.IsName(IdleState) && stateInfo.normalizedTime > 0.1f)
         {
-            animator.SetTrigger("Attack1");
+            animator.SetInteger("Attack", 1);
+            Debug.Log("attack1  is  on");
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (stateInfo.IsName(Attack1State) && stateInfo.normalizedTime > 0.8f)
         {
-            animator.SetTrigger("Act1ToAct2");
+            animator.SetInteger("Attack", 2);
+            Debug.Log("attack2  is  on");
+        }
+        if (stateInfo.IsName(Attack2State) && stateInfo.normalizedTime > 0.8f)
+        {
+            animator.SetInteger("Attack", 3);
+            Debug.Log("attack3  is  on");
         }
     }
 }
