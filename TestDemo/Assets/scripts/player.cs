3e7e48eb2faffb484ba16faa3b8c08ac8741ff7f
zzhehe <460475523@qq.com>
2018-04-03 00:08:58 +0800

    change
    change

--- TestDemo/Assets/scripts/player.cs
+++ TestDemo/Assets/scripts/player.cs
@@ -12,17 +12,27 @@ public class player : MonoBehaviour {
     private const string Attack1State = "Attack1";
     private const string Attack2State = "Attack2";
 
+    private BoxCollider2D colliderPlayer;
+    public Rigidbody2D rigidbodyPlayer;
 
+    public float walkspeed = 10.0f;
+    private float H;
+    private float V;
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
+        colliderPlayer= GetComponent<BoxCollider2D>();
+       // rigidbodyPlayer= GetComponent<Rigidbody2D>();
+
         animator.SetBool("Run", false);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        MovePlayer();
+        //在这里取得移动
+        H = Input.GetAxis("Horizontal");
+        V = Input.GetAxis("Vertical");
 
         stateInfo = animator.GetCurrentAnimatorStateInfo(0);
         if (!stateInfo.IsName(IdleState) && stateInfo.normalizedTime > 0.8f)
@@ -35,9 +45,30 @@ public class player : MonoBehaviour {
         }
     }
 
-    private void MovePlayer()
+    private void FixedUpdate()
     {
-        throw new NotImplementedException();
+        //在fixupdate中改变刚体受力
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Mathf.Abs(H) > 0.1 || Mathf.Abs(V) > 0.1)
+        {
+            Debug.Log("move  is  on H:" + H + "   V:" + V);
+            Vector2 movement = new Vector2(H, V);
+            // rigidbodyPlayer.AddForce(movement * walkspeed);
+            if (stateInfo.IsName(IdleState)|| stateInfo.IsName("Run"))
+            {
+                animator.SetBool("Run", true);
+                transform.Translate(movement * walkspeed);
+            }
+            
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
     }
 
     private void Attack()
@@ -45,17 +76,14 @@ public class player : MonoBehaviour {
         if (stateInfo.IsName(IdleState) && stateInfo.normalizedTime > 0.1f)
         {
             animator.SetInteger("Attack", 1);
-            Debug.Log("attack1  is  on");
         }
-        if (stateInfo.IsName(Attack1State) && stateInfo.normalizedTime > 0.8f)
+        if (stateInfo.IsName(Attack1State) && stateInfo.normalizedTime > 0.6f)
         {
             animator.SetInteger("Attack", 2);
-            Debug.Log("attack2  is  on");
         }
-        if (stateInfo.IsName(Attack2State) && stateInfo.normalizedTime > 0.8f)
+        if (stateInfo.IsName(Attack2State) && stateInfo.normalizedTime > 0.6f)
         {
             animator.SetInteger("Attack", 3);
-            Debug.Log("attack3  is  on");
         }
     }
 }
